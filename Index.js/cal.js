ab62895182377617b7af9d9d8d36d99cdef02df7
ajith-zlr <ajith@zelarsoft.com>
2022-07-14 10:41:38 +0530

    firstcommit

---
+++ Index.js/cal.js
@@ -0,0 +1,50 @@
+let contain=document.getElementById("screen").value;
+
+function addvalue(num){
+
+    contain+=num;
+    document.getElementById("screen").value=contain;
+
+    
+}
+function equal(num){
+
+    let contain=document.getElementById("screen").value;
+
+    document.getElementById("screen").value=eval(contain);
+}
+
+// function del(DEL){
+//     let contain=document.getElementById("screen").value;
+//     tl=contain.length-1;
+//     contain.charAt(tl)=null;
+//     document.getElementById("screen").value=contain;
+// }
+// function equal(num){
+//     try{
+//         contain=eval(contain);
+//     }catch{
+//         alert("throw error");
+//     }
+
+// function del(){
+//     contain.value=contain.value.slice(0,-1);
+// }
+function clr(){
+    document.getElementById("screen").value="";
+    
+}
+function Root(){
+    contain
+
+document.getElementById("screen").value;
+
+}
+
+
+
+
+
+
+    
+
